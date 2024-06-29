8c8b2a5fad1139a192a908924e947a0254ab33ce
sunil <skpal993122@gmail.com>
2024-06-29 18:08:20 +0530

    aaa

--- src/components/CompletedTask.tsx
+++ src/components/CompletedTask.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CompletedTask:React.FC = () => {
   return (
     <div>
-      nhghghj
+      Meri Jaan
     </div>
   )
 }
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import './TaskList.scss'
 
 const TaskList:React.FC=()=> {
   return (
-    <div>
-      hhjhiui
+    <>
+    <div className='task_list_section'>
+
+      <div className='task_list_item'>
+        <input type='checkbox' title='check' className='task_checkbox'></input>
+        <input type='text' title='text' className='task_input'></input>
+        <button type='button' title='subbmit'>Remove</button>
+
+      </div>
     </div>
+      I love you
+    </>
   )
 }
 
